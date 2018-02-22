ef52dbe334fce62ff4aa4c4ad86d22229fe4030a
Mikhail Elfimov <melfimov@changers.team>
2018-02-22 21:16:10 +0300

    PB-3102 microservice boilerplate

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import 'source-map-support/register';
-import * as hardRejection from 'hard-rejection';
+import * as util from 'util';
 
 // const tsConfig = require('../tsconfig.json');
 // const tsConfigPaths = require('tsconfig-paths');
@@ -11,7 +11,31 @@ import * as hardRejection from 'hard-rejection';
 //   paths: tsConfig.compilerOptions.paths,
 // });
 
-hardRejection();
+function padDatePart(str: number) {
+  return ('0' + str).slice(-2);
+}
+
+function consoleErrorTimestamp(message: any) {
+  const now = new Date();
+  console.error(
+    /* tslint:disable:max-line-length */
+    `${now.getFullYear()}-${padDatePart(now.getMonth() + 1)}-${padDatePart(now.getDate())} ${padDatePart(now.getHours())}:${padDatePart(now.getMinutes())}:${padDatePart(now.getSeconds())}`,
+    /* tslint:enable:max-line-length */
+    message,
+  );
+}
+
+process.on('unhandledRejection', (err) => {
+  let message = err;
+  if (!(message instanceof Error)) {
+    message = new Error(`Promise rejected with value: ${util.inspect(err)}`);
+  }
+  consoleErrorTimestamp(message);
+});
+
+process.on('uncaughtException', (err) => {
+  consoleErrorTimestamp(err);
+});
 
 if (!process.env.REPLACE_ME_ENV) {
   throw new Error('Environment variable REPLACE_ME_ENV is not set');
--- src/vendor.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'hard-rejection' {
-  function install(log?: (stack: any) => void): any;
-
-  namespace install {}
-  export = install;
-}
